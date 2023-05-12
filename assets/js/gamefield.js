238aa6d0c3900baca9959c1ec925befb891465fa
zstoimchev <zivkostoimcev1551@outlook.com>
2023-05-13 00:58:30 +0200

    createt table with js

--- assets/js/gamefield.js
+++ assets/js/gamefield.js
@@ -1,10 +1,17 @@
-document.write("<table>")
-for (let i = 1; i <= 9; i++) {
-    document.write("<tr>");
-    for (let j = 1; j <= 9; j++) {
-        document.write("<td id=" + parseInt(i * 10 + j) + ">" + parseInt(i * 10 + j) + "</td>");
+function createTable() {
+    document.write("<table>")
+    for (let i = 1; i <= 9; i++) {
+        document.write("<tr>");
+        for (let j = 1; j <= 9; j++) {
+            document.write('<td id="11" onclick="revealTile(this)"><a href="#"><img src="assets/img/SVGs/1s.svg" alt="one"></a></td>');
+        }
+        document.write("</tr>");
     }
-    document.write("</tr>");
+    document.write("</table>")
 }
-document.write("</table>")
 
+// ===========================================================
+
+function revealTile(){
+    
+}
