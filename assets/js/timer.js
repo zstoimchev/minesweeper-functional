c554084bc5367110ce72ddaddcd60f921f9933f0
zstoimchev <zivkostoimcev1551@outlook.com>
2023-05-14 23:29:04 +0200

    added animations on game (fixed them)

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -29,16 +29,67 @@ function incrementTimeByOneFraction() {
     document.getElementById("timer").innerHTML = makeTimerText(TotalSecFractions);
 }
 
+function mouseOnInstructions () {
+    document.getElementsByClassName("instructions")[0].style.animation = "fade-in-game-elements 1s ease-in-out forwards";
+}
+
+function mouseOutInstructions () {
+    document.getElementsByClassName("instructions")[0].style.animation = "fade-out-game-elements 1s ease-in-out forwards";
+}
+
+function mouseOnStatistics () {
+    document.getElementsByClassName("statistics")[0].style.animation = "fade-in-game-elements 1s ease-in-out forwards";
+}
+
+function mouseOutStatistics () {
+    document.getElementsByClassName("statistics")[0].style.animation = "fade-out-game-elements 1s ease-in-out forwards";
+}
+
+
 function timerStart() {
     if (timerStarted == true)
         return;
     timerInterval = stimerInterval = setInterval(incrementTimeByOneFraction, 100);
     timerStarted = true;
+    document.getElementsByClassName("instructions")[0].style.animation = "fade-out-game-elements 1s ease-in-out forwards";
+    document.getElementsByClassName("statistics")[0].style.animation = "fade-out-game-elements 2s ease-in-out forwards";
+
+
+    document.getElementsByClassName("instructions")[0].addEventListener("mouseenter", mouseOnInstructions);
+    document.getElementsByClassName("instructions")[0].addEventListener("mouseleave", mouseOutInstructions);
+
+    document.getElementsByClassName("statistics")[0].addEventListener("mouseenter", mouseOnStatistics);
+    document.getElementsByClassName("statistics")[0].addEventListener("mouseleave", mouseOutStatistics);
+
+
 }
+
+
+
+
+
+
 
 function timerStop() {
     clearInterval(timerInterval);
     timerStarted = false;
+    document.getElementsByClassName("instructions")[0].style.animation = "fade-in-game-elements 1s ease-in-out forwards";
+    document.getElementsByClassName("statistics")[0].style.animation = "fade-in-game-elements 1s ease-in-out forwards";
+    
+    
+    document.getElementsByClassName("instructions")[0].style.opacity=1;
+    document.getElementsByClassName("statistics")[0].style.opacity=1;
+
+
+    document.getElementsByClassName("instructions")[0].removeEventListener("mouseenter", mouseOnInstructions);
+    document.getElementsByClassName("instructions")[0].removeEventListener("mouseleave", mouseOutInstructions);
+
+    document.getElementsByClassName("statistics")[0].removeEventListener("mouseenter", mouseOnStatistics);
+    document.getElementsByClassName("statistics")[0].removeEventListener("mouseleave", mouseOutStatistics);
+
+
+
+
 }
 
 function reset() {
